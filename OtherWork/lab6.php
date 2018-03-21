4057f7603fc3b5a6f22acbdc113ca35866e79f15
Katie Brandao <kbrandao15@georgefox.edu>
2018-03-21 14:55:43 -0700

    testing for lab6

--- OtherWork/lab6.php
+++ OtherWork/lab6.php
@@ -27,7 +27,7 @@
       print_r($newList);  
       echo "<br />";
       array_unshift($newList[$j], $char);
-      if (sizeof($newList[$j] == sizeof($string))
+      if (sizeof($newList[$j]) == sizeof($string))
       {
          array_push($fullLists, $newList[$j]);  
       }
